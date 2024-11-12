e69852ed65f0f6031df34c17050ddffe9be764e5
Skqlixur <jmjmbo5@gmail.com>
2024-11-12 22:19:15 +0800

    Use JS to populate grid, add assets to board, add rank and file labels.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Chessboard with Flexbox and With Grid
+# Chessboard with Flexbox
 
-This is a chessboard made with HTML, CSS, and JavaScript using both Flexbox and Grid as practice.
+This is a chessboard made with HTML, CSS, and JavaScript using Fl`exbox as practice.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,52 @@
 const flexboxChessboard = document.querySelector('.flex-chessboard')
 
+const rowRanksPiecesPlacementImageNames = [
+  [
+    'black_rook',
+    'black_knight',
+    'black_bishop',
+    'black_queen',
+    'black_king',
+    'black_bishop',
+    'black_knight',
+    'black_rook',
+  ],
+  [
+    'black_pawn',
+    'black_pawn',
+    'black_pawn',
+    'black_pawn',
+    'black_pawn',
+    'black_pawn',
+    'black_pawn',
+    'black_pawn',
+  ],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  [
+    'white_pawn',
+    'white_pawn',
+    'white_pawn',
+    'white_pawn',
+    'white_pawn',
+    'white_pawn',
+    'white_pawn',
+    'white_pawn',
+  ],
+  [
+    'white_rook',
+    'white_knight',
+    'white_bishop',
+    'white_queen',
+    'white_king',
+    'white_bishop',
+    'white_knight',
+    'white_rook',
+  ],
+]
+
 function populateFlexChessboard(flexBoard) {
   const rows = 8
   const cols = 8
@@ -7,10 +54,24 @@ function populateFlexChessboard(flexBoard) {
   for (let i = 0; i < rows; i++) {
     const row = document.createElement('div')
     row.classList.add('flex-chessboard-row')
+
     for (let j = 0; j < cols; j++) {
+      let currentSquare = rowRanksPiecesPlacementImageNames[i][j]
+
       const col = document.createElement('div')
       col.classList.add('flex-chessboard-item')
       row.appendChild(col)
+
+      if (currentSquare === '') continue
+
+      let pieceImage = document.createElement('img')
+      pieceImage.src = `../assets/img/${currentSquare.slice(
+        0,
+        5
+      )}/${currentSquare}.png`
+      pieceImage.classList.add('piece-image')
+
+      col.appendChild(pieceImage)
     }
     flexBoard.appendChild(row)
   }
